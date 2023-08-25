ed23df1fc87c858d76a6312a161b788ec58dfa60
balthapaixao <balthazarpaixao@gmail.com>
2023-08-25 16:56:30 -0300

    feat: adding extraction steps

--- scripts/utils/extraction_utilities.py
+++ scripts/utils/extraction_utilities.py
@@ -1,4 +1,25 @@
 import pandas as pd
+from mplsoccer import Sbopen
+
+
+def parse_event(match_id: int) -> pd.DataFrame:
+    """
+    Extract match data
+    """
+    sb = Sbopen()
+    events, related, freeze, players = sb.event(match_id)
+
+    return events, related, freeze, players
+
+
+def parse_360(match_id: int) -> pd.DataFrame:
+    """
+    Extract match data
+    """
+    sb = Sbopen()
+    frames, visible = sb.frame(match_id)
+
+    return frames, visible
 
 
 def extract_data() -> pd.DataFrame:
